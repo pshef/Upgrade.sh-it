bb23b776bb4b1850213e7685d4a3dc14328d0aa8
pshef <pshef@pm.me>
2022-02-04 01:41:14 +0000

    Added script

---
+++ upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# defining shit
+
+user=$(whoami)
+here=$(pwd)
+time=$(date)
+
+# message
+
+echo -e "If you're running this, you're probably seeing an update notification. Run this to upgrade your shit!\n\n"
+
+# apt repository upgrades
+sudo apt update -qq
+sudo apt upgrade -y --allow-downgrades -qq
+sudo apt full-upgrade -y --allow-downgrades -qq
+sudo apt dist-upgrade -y --allow-downgrades -qq
+sudo apt autoremove -y -qq
+
+# flatpak upgrades
+flatpak update -y
+
+clear
+
+echo "Thanks ${user}, you have finished updating all your software at ${time} and ran this command in ${here}. Thank you for updating.sh(it)!"
